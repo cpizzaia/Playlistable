4885bdd52e2387157f744a02c0794d785a8e4544
Cody Pizzaia <cpizzaia@asu.edu>
2017-10-22 12:52:23 -0400

    add request methods and translator

---
+++ Playlistable/Networking/SpotifyTranslator.swift
@@ -0,0 +1,31 @@
+//
+//  SpotifyTranslator.swift
+//  Playlistable
+//
+//  Created by Cody Pizzaia on 10/22/17.
+//  Copyright © 2017 Cody Pizzaia. All rights reserved.
+//
+
+import Foundation
+
+struct SpotifyTranslator {
+  // MARK: Public Static Methods
+  static func translateToSuccessResponse(response: SpotifyRequest.TracksResponse) -> APIRequest.SuccessResponse {
+    return { data in
+      
+    }
+  }
+  
+  static func translateToSuccessResponse(response: SpotifyRequest.ArtistsResponse) -> APIRequest.SuccessResponse {
+    return { data in
+      
+    }
+  }
+  
+  static func translateToSuccessResponse(response: SpotifyRequest.AlbumsResponse) -> APIRequest.SuccessResponse {
+    return { data in
+      
+    }
+  }
+  
+}
