bd02db0a122ac93802c37760148068b46da268ea
Cody Pizzaia <cpizzaia@asu.edu>
2017-10-23 23:41:43 -0400

    load view controller from nib

--- Playlistable/AppDelegate.swift
+++ Playlistable/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     window = UIWindow(frame: UIScreen.main.bounds)
     
-    let viewController = ViewController(nibName: "ViewController", bundle: .main)
+    let viewController = Bundle.main.loadNibNamed("ViewController", owner: nil)?.first as! ViewController
     
     window?.rootViewController = viewController
     
