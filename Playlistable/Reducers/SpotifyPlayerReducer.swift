bbd9b0b0c83fdf4859cc8e5c6cc8a3006f66ea01
Cody Pizzaia <cpizzaia@asu.edu>
2017-11-12 23:09:26 -0500

    add playing track ID state

--- Playlistable/Reducers/SpotifyPlayerReducer.swift
+++ Playlistable/Reducers/SpotifyPlayerReducer.swift
@@ -11,16 +11,19 @@ import ReSwift
 
 struct SpotifyPlayerState {
   var isInitialized: Bool
+  var playingTrackID: String?
 }
 
-fileprivate let initialSpotifyPlayerState = SpotifyPlayerState(isInitialized: false)
+fileprivate let initialSpotifyPlayerState = SpotifyPlayerState(isInitialized: false, playingTrackID: nil)
 
 func spotifyPlayerReducer(action: Action, state: SpotifyPlayerState?) -> SpotifyPlayerState{
   var state = state ?? initialSpotifyPlayerState
   
   switch action {
-  case _ as InitializedPlayer :
+  case _ as InitializedPlayer:
     state.isInitialized = true
+  case let action as PlayingTrack:
+    state.playingTrackID = action.trackID
   default:
     break
   }
